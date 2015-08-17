cd4c802cffe4827cb6fa26cefc80d481211a75a0
bijiabo <bijiabo@gmail.com>
2015-08-18 02:29:27 +0800

    update

--- GrowUpDaily/AppDelegate.swift
+++ GrowUpDaily/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window?.rootViewController? = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("grownScrollView") 
+        
         return true
     }
 
